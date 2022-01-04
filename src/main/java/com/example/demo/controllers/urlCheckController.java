ca625aa865ee91c614d7e0d041301e7921d239d4
pauljflanagan <pflan414@gmail.com>
2022-01-04 13:07:07 -0500

    first commit

---
+++ src/main/java/com/example/demo/controllers/urlCheckController.java
@@ -0,0 +1,41 @@
+package com.example.demo.controllers;
+
+import java.io.IOException;
+import java.net.HttpURLConnection;
+import java.net.MalformedURLException;
+import java.net.URL;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class urlCheckController {
+    private final String SITE_IS_UP = "Site is up!";
+    private final String SITE_IS_DOWN = "Site is down!";
+    private final String INCORRECT_URL = "URL is incorrect!";
+
+    @GetMapping("/check")
+    public String getUrlStatusMsg(@RequestParam String url) {
+        String returnMessage = "";
+        try {    
+            URL urlObj = new URL(url);
+            HttpURLConnection conn = (HttpURLConnection) urlObj.openConnection();
+            conn.setRequestMethod("GET");
+            conn.connect();
+            int responseCodeCategory = conn.getResponseCode() / 100;
+            if (responseCodeCategory >= 4) {
+                returnMessage = SITE_IS_DOWN; 
+            }
+            else {
+                returnMessage = SITE_IS_UP;
+            }
+        } catch(MalformedURLException e) {
+            returnMessage = INCORRECT_URL;        
+        } catch (IOException e) {
+            returnMessage = SITE_IS_DOWN;
+        }
+        return returnMessage;
+    }
+    
+}
